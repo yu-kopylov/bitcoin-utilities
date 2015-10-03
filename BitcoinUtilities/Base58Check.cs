a22ccbba9ba83550e17e52a3ee7458829be5de49
Yuriy Kopylov <iyurij@gmail.com>
2015-10-03 05:59:27 +0500

    Improved Base58Check.Encode performance.

--- BitcoinUtilities/Base58Check.cs
+++ BitcoinUtilities/Base58Check.cs
@@ -10,6 +10,7 @@ namespace BitcoinUtilities
     public static class Base58Check
     {
         private const string Alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+        private static readonly BigInteger Base58Pow8 = BigInteger.Pow(58, 8);
 
         /// <summary>
         /// Encodes the given bytes using the Base58Check encoding.<br/>
@@ -27,7 +28,8 @@ namespace BitcoinUtilities
                 byte[] hash = sha256Alg.ComputeHash(sha256Alg.ComputeHash(data));
                 Array.Copy(hash, 0, buf, data.Length, 4);
             }
-            return EncodeNoCheckcode(buf);
+
+            return EncodeNoCheck(buf);
         }
 
         /// <summary>
@@ -36,22 +38,29 @@ namespace BitcoinUtilities
         /// </summary>
         /// <param name="data">Bytes to encode.</param>
         /// <returns>Results of encodung.</returns>
-        internal static string EncodeNoCheckcode(byte[] data)
+        internal static string EncodeNoCheck(byte[] data)
         {
-            byte[] unsignedData = new byte[(data.Length + 7)*8/5];
+            byte[] unsignedData = new byte[data.Length + 1];
             Array.Copy(data, 0, unsignedData, 0, data.Length);
             Array.Reverse(unsignedData, 0, data.Length);
             BigInteger value = new BigInteger(unsignedData);
 
-            int maxResultLength = data.Length*5 + 1;
+            int maxResultLength = 1 + data.Length*7/5; // Log(256, 58) = 1.365658237
             char[] res = new char[maxResultLength];
             int firstCharOfs = maxResultLength;
 
             while (value != 0)
             {
-                --firstCharOfs;
-                res[firstCharOfs] = Alphabet[(int) (value%58)];
-                value /= 58;
+                BigInteger bigReminder;
+                value = BigInteger.DivRem(value, Base58Pow8, out bigReminder);
+                ulong reminder = (ulong) bigReminder;
+                res[--firstCharOfs] = Alphabet[(int) (reminder%58)];
+                reminder /= 58;
+                for (int i = 0; reminder != 0 || (!value.IsZero && i < 7); i++)
+                {
+                    res[--firstCharOfs] = Alphabet[(int) (reminder%58)];
+                    reminder /= 58;
+                }
             }
 
             for (int leadingZeros = 0; leadingZeros < data.Length && data[leadingZeros] == 0; leadingZeros++)
--- Test.BitcoinUtilities/TestBase58Check.cs
+++ Test.BitcoinUtilities/TestBase58Check.cs
@@ -1,4 +1,6 @@
-﻿using BitcoinUtilities;
+﻿using System;
+using System.Numerics;
+using BitcoinUtilities;
 using NUnit.Framework;
 
 namespace Test.BitcoinUtilities
@@ -7,16 +9,63 @@ namespace Test.BitcoinUtilities
     public class TestBase58Check
     {
         [Test]
-        public void TestEncodeNoCheckcode()
+        public void TestEncodeNoCheck()
         {
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {}), Is.EqualTo(""));
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {0x00}), Is.EqualTo("1"));
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {0x00, 0x00}), Is.EqualTo("11"));
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {0x00, 0x00, 0x00, 0x00, 0x00}), Is.EqualTo("11111"));
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {0x01, 0xFF}), Is.EqualTo("9p"));
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {0xFF, 0x01}), Is.EqualTo("LQY"));
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {0x00, 0x00, 0x00, 0x01, 0xFF}), Is.EqualTo("1119p"));
-            Assert.That(Base58Check.EncodeNoCheckcode(new byte[] {0x00, 0x00, 0x00, 0xFF, 0x01}), Is.EqualTo("111LQY"));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {}), Is.EqualTo(""));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {0x00}), Is.EqualTo("1"));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {0x00, 0x00}), Is.EqualTo("11"));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {0x00, 0x00, 0x00, 0x00, 0x00}), Is.EqualTo("11111"));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {0x01, 0xFF}), Is.EqualTo("9p"));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {0xFF, 0x01}), Is.EqualTo("LQY"));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {0x00, 0x00, 0x00, 0x01, 0xFF}), Is.EqualTo("1119p"));
+            Assert.That(Base58Check.EncodeNoCheck(new byte[] {0x00, 0x00, 0x00, 0xFF, 0x01}), Is.EqualTo("111LQY"));
+        }
+
+        [Test]
+        public void TestEncodeNoCheck58Pow()
+        {
+            for (int i = 1; i < 128; i++)
+            {
+                string str = "2".PadRight(i + 1, '1');
+                BigInteger val = BigInteger.Pow(58, i);
+                Assert.That(Base58Check.EncodeNoCheck(ToByteArray(val)), Is.EqualTo(str), "i = " + i);
+            }
+        }
+
+        [Test]
+        public void TestEncodeNoCheck58PowMinus1()
+        {
+            for (int i = 1; i < 128; i++)
+            {
+                string str = "".PadRight(i, 'z');
+                BigInteger val = BigInteger.Pow(58, i) - 1;
+                Assert.That(Base58Check.EncodeNoCheck(ToByteArray(val)), Is.EqualTo(str), "i = " + i);
+            }
+        }
+
+        [Test]
+        public void TestEncodeNoCheck58PowPlus1()
+        {
+            for (int i = 1; i < 128; i++)
+            {
+                string str = "2".PadRight(i, '1') + "2";
+                BigInteger val = BigInteger.Pow(58, i) + 1;
+                Assert.That(Base58Check.EncodeNoCheck(ToByteArray(val)), Is.EqualTo(str), "i = " + i);
+            }
+        }
+
+        private static byte[] ToByteArray(BigInteger value)
+        {
+            byte[] bytes = value.ToByteArray();
+            Array.Reverse(bytes);
+            int firstByte = 0;
+            while (bytes[firstByte] == 0)
+            {
+                firstByte++;
+            }
+            byte[] truncatedBytes = new byte[bytes.Length - firstByte];
+            Array.Copy(bytes, firstByte, truncatedBytes, 0, bytes.Length - firstByte);
+            return truncatedBytes;
         }
 
         [Test]
@@ -42,6 +91,20 @@ namespace Test.BitcoinUtilities
                     0x01, 0x42, 0xC0, 0xF4, 0xE7, 0x75, 0xA8, 0x40, 0x20, 0xB1, 0xF0, 0xCA, 0x28, 0x53, 0x28, 0x7A, 0xCA, 0xC1, 0x1A, 0x43,
                     0x03, 0x2E, 0x27, 0x4E, 0x54, 0x36, 0x92, 0xBE, 0x5F, 0xA2, 0xB8, 0x54, 0x32, 0x67, 0x15, 0x43, 0xC1, 0xAC, 0x0E
                 }), Is.EqualTo("6PRW5o9FLp4gJDDVqJQKJFTpMvdsSGJxMYHtHaQBF3ooa8mwD69bapcDQn"));
+        }
+
+        [Test]
+        public void TestEncodePerformance()
+        {
+            byte[] privateKey = new byte[33];
+            privateKey[0] = 0x80;
+            privateKey[1] = 0x7F;
+            for (int i = 0; i < 10000; i++)
+            {
+                privateKey[2] = (byte) i;
+                privateKey[3] = (byte) (i/256);
+                Base58Check.Encode(privateKey);
+            }
         }
     }
 }
