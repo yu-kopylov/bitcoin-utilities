5e63bcb5f564b953fc6876c514356af7bf4a6964
Yuriy Kopylov <iyurij@gmail.com>
2016-01-24 01:30:12 +0500

    Added template for VirtualDictionary.

---
+++ BitcoinUtilities/Collections/VirtualDictionary/VirtualDictionary.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BitcoinUtilities.Collections
+{
+    public partial class VirtualDictionary : IDisposable
+    {
+        //todo: use to accumulate small changes
+        private readonly List<NonIndexedRecord> nonIndexedRecords = new List<NonIndexedRecord>();
+        //todo: use local comparer
+        private readonly Dictionary<byte[], int> nonIndexedRecordsByKey = new Dictionary<byte[], int>(ByteArrayComparer.Instance);
+
+        private VirtualDictionary(string filename, int keySize, int valueSize)
+        {
+        }
+
+        public static VirtualDictionary Open(string filename, int keySize, int valueSize)
+        {
+            return new VirtualDictionary(filename, keySize, valueSize);
+        }
+
+        public void Dispose()
+        {
+            //todo: implement
+        }
+
+        public VirtualDictionaryTransaction BeginTransaction()
+        {
+            return new VirtualDictionaryTransaction(this);
+        }
+
+        //todo: class or struct?
+        private class NonIndexedRecord
+        {
+            private readonly byte[] key;
+            private readonly byte[] value;
+
+            public NonIndexedRecord(byte[] key, byte[] value)
+            {
+                this.key = key;
+                this.value = value;
+            }
+
+            public byte[] Key
+            {
+                get { return key; }
+            }
+
+            public byte[] Value
+            {
+                get { return value; }
+            }
+        }
+
+        internal bool TryGetNonIndexedValue(byte[] key, out byte[] value)
+        {
+            int recordIndex;
+            if (nonIndexedRecordsByKey.TryGetValue(key, out recordIndex))
+            {
+                value = nonIndexedRecords[recordIndex].Value;
+                return true;
+            }
+            value = null;
+            return false;
+        }
+
+        public void AddNonIndexedValue(byte[] key, byte[] value)
+        {
+            //todo: implement correctly (depends on record being class or struct)
+            NonIndexedRecord record = new NonIndexedRecord(key, value);
+            int recordIndex;
+            if (nonIndexedRecordsByKey.TryGetValue(key, out recordIndex))
+            {
+                nonIndexedRecords[recordIndex] = record;
+            }
+            else
+            {
+                recordIndex = nonIndexedRecords.Count;
+                nonIndexedRecords.Add(record);
+                nonIndexedRecordsByKey.Add(key, recordIndex);
+            }
+        }
+    }
+
+    public class VirtualDictionaryTransaction : IDisposable
+    {
+        private readonly VirtualDictionary dictionary;
+
+        //todo: use local comparer
+        private readonly Dictionary<byte[], byte[]> unsavedRecords = new Dictionary<byte[], byte[]>(ByteArrayComparer.Instance);
+
+        internal VirtualDictionaryTransaction(VirtualDictionary dictionary)
+        {
+            this.dictionary = dictionary;
+        }
+
+        public void Dispose()
+        {
+            Rollback();
+        }
+
+        public void AddOrUpdate(byte[] key, byte[] value)
+        {
+            //todo: validate key and value length
+            unsavedRecords[key] = value;
+        }
+
+        public Dictionary<byte[], byte[]> Find(List<byte[]> keys)
+        {
+            //todo: use local comparer
+            Dictionary<byte[], byte[]> res = new Dictionary<byte[], byte[]>(ByteArrayComparer.Instance);
+
+            List<byte[]> keysToLookup = new List<byte[]>();
+
+            foreach (byte[] key in keys)
+            {
+                byte[] value;
+                if (unsavedRecords.TryGetValue(key, out value))
+                {
+                    res.Add(key, value);
+                }
+                else if (dictionary.TryGetNonIndexedValue(key, out value))
+                {
+                    res.Add(key, value);
+                }
+                else
+                {
+                    keysToLookup.Add(key);
+                }
+            }
+
+            //todo: implement search for saved keys
+
+            return res;
+        }
+
+        public void Commit()
+        {
+            //todo: implement
+            foreach (KeyValuePair<byte[], byte[]> unsavedRecord in unsavedRecords)
+            {
+                dictionary.AddNonIndexedValue(unsavedRecord.Key, unsavedRecord.Value);
+            }
+        }
+
+        public void Rollback()
+        {
+            //todo: inplement
+        }
+    }
+}
---
+++ Test.BitcoinUtilities/Collections/TestVirtualDictionary.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using BitcoinUtilities.Collections;
+using NUnit.Framework;
+
+namespace Test.BitcoinUtilities.Collections
+{
+    [TestFixture]
+    public class TestVirtualDictionary
+    {
+        private string testFolder;
+
+        [SetUp]
+        public void Setup()
+        {
+            testFolder = Path.GetFullPath("tmp-test-VD");
+
+            Console.WriteLine("Removing files in the test folder: : {0}", testFolder);
+
+            Directory.CreateDirectory(testFolder);
+            foreach (string pattern in new string[]{"*.tbl", "*.tbl-wal"})
+            {
+                foreach (string filename in Directory.GetFiles(testFolder, pattern, SearchOption.TopDirectoryOnly))
+                {
+                    Console.WriteLine("Removing: {0}", filename);
+                    File.Delete(filename);
+                }
+            }
+        }
+
+        [Test]
+        [Explicit]
+        public void TestPerformance()
+        {
+            //todo: use temp file
+            //todo: is *.tbl extension save (does not cause problems like *.sdb)
+            using (VirtualDictionary dict = VirtualDictionary.Open(Path.Combine(testFolder, "perf.tbl"), 20, 8))
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+
+                using (var tx = dict.BeginTransaction())
+                {
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        tx.AddOrUpdate(CreateKey(i), BitConverter.GetBytes((long) i));
+                    }
+                    tx.Commit();
+                }
+
+                Console.WriteLine("Insert took: {0}ms.", sw.ElapsedMilliseconds);
+                sw.Restart();
+
+                using (var tx = dict.BeginTransaction())
+                {
+                    for (int i = 1000000; i < 2000000; i++)
+                    {
+                        tx.AddOrUpdate(CreateKey(i), BitConverter.GetBytes((long) i));
+                    }
+                    tx.Commit();
+                }
+
+                Console.WriteLine("Insert took: {0}ms.", sw.ElapsedMilliseconds);
+                sw.Restart();
+
+                using (var tx = dict.BeginTransaction())
+                {
+                    for (int i = 1900000; i < 2100000; i++)
+                    {
+                        tx.AddOrUpdate(CreateKey(i), BitConverter.GetBytes((long) i));
+                    }
+                    tx.Commit();
+                }
+
+                Console.WriteLine("Insert with update took: {0}ms.", sw.ElapsedMilliseconds);
+                sw.Restart();
+
+                using (var tx = dict.BeginTransaction())
+                {
+                    for (int i = 15000; i < 20000; i++)
+                    {
+                        tx.AddOrUpdate(CreateKey(i), BitConverter.GetBytes((long) i));
+                    }
+                    tx.Commit();
+                }
+
+                Console.WriteLine("Small update took: {0}ms.", sw.ElapsedMilliseconds);
+                sw.Restart();
+
+                using (var tx = dict.BeginTransaction())
+                {
+                    List<byte[]> keys = new List<byte[]>();
+                    for (int i = 15000; i < 20000; i++)
+                    {
+                        keys.Add(CreateKey(i));
+                    }
+
+                    keys.Add(CreateKey(999999999));
+
+                    Dictionary<byte[], byte[]> values = tx.Find(keys);
+                    Assert.That(values.Count, Is.EqualTo(keys.Count - 1));
+
+                    for (int i = 15000; i < 20000; i++)
+                    {
+                        byte[] key = CreateKey(i);
+                        Assert.That(values[key], Is.EqualTo(BitConverter.GetBytes((long) i)));
+                    }
+
+                    tx.Commit();
+                }
+
+                Console.WriteLine("Lookup took: {0}ms.", sw.ElapsedMilliseconds);
+                sw.Restart();
+            }
+        }
+
+        [Test]
+        public void TestSimpleAdd()
+        {
+            using (VirtualDictionary dict = VirtualDictionary.Open(Path.Combine(testFolder, "TestSimpleAdd.tbl"), 1, 1))
+            {
+                using (var tx = dict.BeginTransaction())
+                {
+                    Dictionary<byte[], byte[]> foundValues = tx.Find(new List<byte[]> { new byte[] { 1 } });
+                    Assert.That(foundValues, Is.Empty);
+
+                    tx.AddOrUpdate(new byte[] { 1 }, new byte[] { 1 });
+
+                    foundValues = tx.Find(new List<byte[]> { new byte[] { 1 } });
+                    Assert.That(foundValues.Count, Is.EqualTo(1));
+                    Assert.That(foundValues[new byte[] { 1 }], Is.EqualTo(new byte[] { 1 }));
+
+                    tx.Commit();
+
+                    foundValues = tx.Find(new List<byte[]> { new byte[] { 1 } });
+                    Assert.That(foundValues.Count, Is.EqualTo(1));
+                    Assert.That(foundValues[new byte[] { 1 }], Is.EqualTo(new byte[] { 1 }));
+                }
+
+                using (var tx = dict.BeginTransaction())
+                {
+                    Dictionary<byte[], byte[]> foundValues = tx.Find(new List<byte[]> { new byte[] { 1 } });
+                    Assert.That(foundValues.Count, Is.EqualTo(1));
+                    Assert.That(foundValues[new byte[] { 1 }], Is.EqualTo(new byte[] { 1 }));
+
+                    tx.Commit();
+                }
+            }
+        }
+
+        private static byte[] CreateKey(int val)
+        {
+            byte[] key = new byte[20];
+            for (int i = 0; i < 20; i++)
+            {
+                key[i] = (byte) ((val >> 8*(i%4))*(31 + i/4*8) + (val >> 8*((i + 1)%4))*(33 + i/4*8));
+            }
+            return key;
+        }
+    }
+}
