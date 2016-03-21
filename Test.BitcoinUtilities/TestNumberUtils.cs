b82a6e2f00f17bf675f4ed6d0c7fd13a80e59736
Yuriy Kopylov <iyurij@gmail.com>
2016-03-22 03:15:01 +0500

    Added test for NumberUtils.GetBytes.

--- Test.BitcoinUtilities/TestNumberUtils.cs
+++ Test.BitcoinUtilities/TestNumberUtils.cs
@@ -7,9 +7,21 @@ namespace Test.BitcoinUtilities
     public class TestNumberUtils
     {
         [Test]
-        public void Test()
+        public void TestReverseBytes()
         {
             Assert.That(NumberUtils.ReverseBytes(0x12345678), Is.EqualTo(0x78563412));
+        }
+
+        [Test]
+        public void TestGetBytes()
+        {
+            Assert.That(NumberUtils.GetBytes(0), Is.EqualTo(new byte[] {0, 0, 0, 0}));
+            Assert.That(NumberUtils.GetBytes(1), Is.EqualTo(new byte[] {1, 0, 0, 0}));
+            Assert.That(NumberUtils.GetBytes(-1), Is.EqualTo(new byte[] {0xFF, 0xFF, 0xFF, 0xFF}));
+            Assert.That(NumberUtils.GetBytes(0x12345678), Is.EqualTo(new byte[] {0x78, 0x56, 0x34, 0x12}));
+            Assert.That(
+                NumberUtils.GetBytes(unchecked((int) 0x81828384)),
+                Is.EqualTo(new byte[] {0x84, 0x83, 0x82, 0x81}));
         }
     }
 }
