00786488a7c302520dfda5c6c21955837d170dfc
Yuriy Kopylov <iyurij@gmail.com>
2016-03-23 19:32:24 +0500

    Added BitcoinPrivateKey.IsStrong method to perform additional validation of generated keys.

--- BitcoinUtilities/BitcoinPrivateKey.cs
+++ BitcoinUtilities/BitcoinPrivateKey.cs
@@ -1,4 +1,7 @@
-﻿namespace BitcoinUtilities
+﻿using System.Collections.Generic;
+using Org.BouncyCastle.Math;
+
+namespace BitcoinUtilities
 {
     /// <summary>
     /// Specification: https://en.bitcoin.it/wiki/Private_key
@@ -8,16 +11,16 @@
         public const int KeyLength = 32;
 
         private static readonly byte[] MinValue = new byte[]
-                                                  {
-                                                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                                                      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01
-                                                  };
+        {
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01
+        };
 
         private static readonly byte[] MaxValue = new byte[]
-                                                  {
-                                                      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE,
-                                                      0xBA, 0xAE, 0xDC, 0xE6, 0xAF, 0x48, 0xA0, 0x3B, 0xBF, 0xD2, 0x5E, 0x8C, 0xD0, 0x36, 0x41, 0x40
-                                                  };
+        {
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE,
+            0xBA, 0xAE, 0xDC, 0xE6, 0xAF, 0x48, 0xA0, 0x3B, 0xBF, 0xD2, 0x5E, 0x8C, 0xD0, 0x36, 0x41, 0x40
+        };
 
         /// <summary>
         /// Checks that the given array is a valid private key.
@@ -42,6 +45,62 @@
             {
                 return false;
             }
+            return true;
+        }
+
+        /// <summary>
+        /// Performs checks on the given private key to ensure that it looks random.
+        /// </summary>
+        /// <param name="privateKey">The array of 32 bytes to validate.</param>
+        /// <returns>true if the given byte array is a valid private key that looks random; otherwise, false.</returns>
+        public static bool IsStrong(byte[] privateKey)
+        {
+            if (!IsValid(privateKey))
+            {
+                return false;
+            }
+
+            if (!IsRandomArray(privateKey))
+            {
+                return false;
+            }
+
+            var privateKeyNumber = new BigInteger(1, privateKey);
+            var invPrivateKeyNumber = privateKeyNumber.ModInverse(Secp256K1Curve.GroupSize);
+            byte[] invPrivateKey = invPrivateKeyNumber.ToByteArrayUnsigned();
+
+            if (!IsRandomArray(invPrivateKey))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsRandomArray(byte[] privateKey)
+        {
+            HashSet<byte> distinctBytes = new HashSet<byte>();
+            HashSet<byte> distinctDeltas = new HashSet<byte>();
+            for (int i = 0; i < privateKey.Length; i++)
+            {
+                byte b0 = privateKey[i];
+                byte b1 = privateKey[(i + 1)%privateKey.Length];
+                byte delta = (byte) (b1 - b0);
+
+                distinctBytes.Add(b0);
+                distinctDeltas.Add(delta);
+            }
+
+            if (distinctBytes.Count < 24)
+            {
+                return false;
+            }
+
+            if (distinctDeltas.Count < 24)
+            {
+                return false;
+            }
+
             return true;
         }
 
---
+++ BitcoinUtilities/Secp256K1Curve.cs
@@ -0,0 +1,37 @@
+﻿using Org.BouncyCastle.Asn1.Sec;
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+
+namespace BitcoinUtilities
+{
+    public static class Secp256K1Curve
+    {
+        private static readonly ECPoint g;
+        private static readonly BigInteger fieldSize;
+        private static readonly BigInteger groupSize;
+
+        static Secp256K1Curve()
+        {
+            X9ECParameters parameters = SecNamedCurves.GetByName("secp256k1");
+            g = parameters.G;
+            fieldSize = ((FpCurve)parameters.Curve).Q;
+            groupSize = parameters.N;
+        }
+
+        public static ECPoint G
+        {
+            get { return g; }
+        }
+
+        public static BigInteger FieldSize
+        {
+            get { return fieldSize; }
+        }
+
+        public static BigInteger GroupSize
+        {
+            get { return groupSize; }
+        }
+    }
+}
