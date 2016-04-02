a5dee02f8353e0448afebd5055d96c3f3453fb50
Yuriy Kopylov <iyurij@gmail.com>
2016-04-02 22:58:10 +0500

    Added a list of DNS seeds.

---
+++ BitcoinUtilities/P2P/DnsSeeds.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BitcoinUtilities.P2P
+{
+    public static class DnsSeeds
+    {
+        // source: https://github.com/bitcoinxt/bitcoinxt/blob/0.11E/src/chainparams.cpp
+        private static readonly string[] seeds = new string[]
+        {
+            "seed.bitcoin.sipa.be", // Pieter Wiulle
+            "dnsseed.bluematt.me", // Matt Corallo
+            "dnsseed.bitcoin.dashjr.org", // Luke Dashjr
+            "seed.bitcoinstats.com", // Christian Decker
+            "seed.bitnodes.io", // Addy Yeow
+            "dnsseed.vinumeris.com", // Mike Hearn
+            "seed.bitcoin.jonasschnelli.ch" // Jonas Schnelli
+        };
+
+        /// <summary>
+        /// Queries DNS seeds for nodes' addresses.
+        /// </summary>
+        /// <returns>List of nodes' addresses returned by the seeds.</returns>
+        public static List<IPAddress> GetNodeAddresses()
+        {
+            //todo: differentiate between Main and Test networks
+            HashSet<IPAddress> res = new HashSet<IPAddress>();
+
+            foreach (string dnsSeed in seeds)
+            {
+                var addresses = QueryDns(dnsSeed);
+                foreach (IPAddress address in addresses)
+                {
+                    res.Add(address);
+                }
+            }
+
+            return res.ToList();
+        }
+
+        private static IPAddress[] QueryDns(string dnsSeed)
+        {
+            try
+            {
+                return Dns.GetHostAddresses(dnsSeed);
+            }
+            catch (SocketException)
+            {
+            }
+            return new IPAddress[0];
+        }
+    }
+}
---
+++ Test.BitcoinUtilities/P2P/TestDnsSeeds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using BitcoinUtilities.P2P;
+using NUnit.Framework;
+
+namespace Test.BitcoinUtilities.P2P
+{
+    [TestFixture]
+    public class TestDnsSeeds
+    {
+        [Test]
+        public void Test()
+        {
+            List<IPAddress> addresses = DnsSeeds.GetNodeAddresses();
+            Assert.That(addresses.Count, Is.GreaterThan(8));
+        }
+    }
+}
