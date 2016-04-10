b37c63f0a13fb3c71f6b3b935cd5ff159164982a
Yuriy Kopylov <iyurij@gmail.com>
2016-04-10 18:36:30 +0500

    A minor change to TestVersionMessage.

--- Test.BitcoinUtilities/P2P/Messages/TestVersionMessage.cs
+++ Test.BitcoinUtilities/P2P/Messages/TestVersionMessage.cs
@@ -27,8 +27,8 @@ namespace Test.BitcoinUtilities.P2P.Messages
                 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                 // Node ID
                 0x3B, 0x2E, 0xB3, 0x5D, 0x8C, 0xE6, 0x17, 0x65,
-                // "/Satoshi:0.7.2/" sub-version string (string is 15 bytes long)
-                0x0F, 0x2F, 0x53, 0x61, 0x74, 0x6F, 0x73, 0x68, 0x69, 0x3A, 0x30, 0x2E, 0x37, 0x2E, 0x32, 0x2F,
+                // "/Satoshi:0.9.0/" sub-version string (string is 15 bytes long)
+                0x0F, 0x2F, 0x53, 0x61, 0x74, 0x6F, 0x73, 0x68, 0x69, 0x3A, 0x30, 0x2E, 0x39, 0x2E, 0x30, 0x2F,
                 // Last block sending node has is block #212672
                 0xC0, 0x3E, 0x03, 0x00,
                 // relay=true
@@ -44,7 +44,7 @@ namespace Test.BitcoinUtilities.P2P.Messages
             }
 
             Assert.That(message.ProtocolVersion, Is.EqualTo(70002));
-            Assert.That(message.UserAgent, Is.EqualTo("/Satoshi:0.7.2/"));
+            Assert.That(message.UserAgent, Is.EqualTo("/Satoshi:0.9.0/"));
             Assert.That(message.Services, Is.EqualTo(1));
             Assert.That(message.StartHeight, Is.EqualTo(212672));
             Assert.That(message.AcceptBroadcasts, Is.True);
