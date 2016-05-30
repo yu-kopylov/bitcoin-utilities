ee3f2d21f0e0565ae97b1280dd5207558f65d509
Yuriy Kopylov <iyurij@gmail.com>
2016-05-30 16:12:04 +1000

    Added EventManager to handle multiple events from Blockchain in GUI.

---
+++ BitcoinUtilities.GUI.Models/EventManager.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using NLog;
+
+namespace BitcoinUtilities.GUI.Models
+{
+    /// <summary>
+    /// This class serves as a bridge between sources that produce events frequently and possibly slow event handlers.
+    /// <para/>
+    /// A handler for an event type would be called exactly once for any series of events of same type that happen before handler is called.
+    /// <para/>
+    /// All handlers are called within a single thread that is controled by this class.
+    /// <para/>
+    /// All public methods are thread-safe.
+    /// </summary>
+    public class EventManager : IDisposable
+    {
+        public static readonly int TreadShutdownPeriod = 15000;
+
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
+
+        private readonly object firedEventsLock = new object();
+        private readonly object listenersLock = new object();
+        private readonly object threadLock = new object();
+
+        private readonly HashSet<string> firedEvents = new HashSet<string>();
+        private readonly Dictionary<string, List<EventListener>> eventListeners = new Dictionary<string, List<EventListener>>();
+
+        private Thread thread;
+        private bool started;
+        private readonly AutoResetEvent workerWaitEvent = new AutoResetEvent(false);
+
+        /// <summary>
+        /// Starts the event manager.
+        /// </summary>
+        public void Start()
+        {
+            lock (threadLock)
+            {
+                if (started)
+                {
+                    logger.Warn($"{nameof(EventManager)} was already started.");
+                    return;
+                }
+
+                started = true;
+
+                thread = new Thread(WorkerLoop);
+                thread.IsBackground = true;
+                thread.Name = $"{nameof(EventManager)}@{GetHashCode()}";
+                thread.Start();
+            }
+        }
+
+        /// <summary>
+        /// Stops the event manager.
+        /// <para/>
+        /// If executing thread is busy, then waits for it to stop but no longer then <see cref="TreadShutdownPeriod"/> milliseconds.
+        /// </summary>
+        public void Stop()
+        {
+            Thread threadToWait = null;
+            lock (threadLock)
+            {
+                if (started)
+                {
+                    started = false;
+                    threadToWait = thread;
+                }
+            }
+            if (threadToWait != null)
+            {
+                workerWaitEvent.Set();
+                threadToWait.Join(TreadShutdownPeriod);
+            }
+        }
+
+        /// <summary>
+        /// Stops the event manager.
+        /// <para/>
+        /// If executing thread is busy, then waits for it to stop but no longer then <see cref="TreadShutdownPeriod"/> milliseconds.
+        /// </summary>
+        public void Dispose()
+        {
+            Stop();
+
+            workerWaitEvent.Dispose();
+        }
+
+        /// <summary>
+        /// Informs the EventManager about occured event.
+        /// <para/>
+        /// This method is fast. It never waits any events handlers to accept or process the event.
+        /// </summary>
+        public void Notify(string eventType)
+        {
+            lock (firedEventsLock)
+            {
+                firedEvents.Add(eventType);
+            }
+            workerWaitEvent.Set();
+        }
+
+        /// <summary>
+        /// Adds a handler for a given event type.
+        /// </summary>
+        /// <param name="eventType">The event type for which handler would be registered.</param>
+        /// <param name="action">The action that should be performed when the event happens.</param>
+        public void Watch(string eventType, Action action)
+        {
+            lock (listenersLock)
+            {
+                List<EventListener> listeners;
+                if (!eventListeners.TryGetValue(eventType, out listeners))
+                {
+                    listeners = new List<EventListener>();
+                    eventListeners.Add(eventType, listeners);
+                }
+                if (listeners.Any(l => l.IsActionEquals(action)))
+                {
+                    //todo: throw error instead?
+                    logger.Warn($"The same action was already registered for '{eventType}' event.");
+                }
+                else
+                {
+                    EventListener eventListener = new EventListener(action);
+                    listeners.Add(eventListener);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes a handler for a given event type.
+        /// </summary>
+        /// <param name="eventType">The event type for which handler would be removed.</param>
+        /// <param name="action">The action that was previously registered for the given event type.</param>
+        public void Unwatch(string eventType, Action action)
+        {
+            lock (listenersLock)
+            {
+                List<EventListener> listeners;
+                if (!eventListeners.TryGetValue(eventType, out listeners))
+                {
+                    //todo: throw error instead?
+                    logger.Warn($"No listenters are registered for '{eventType}' event.");
+                    return;
+                }
+                EventListener listener = listeners.FirstOrDefault(l => l.IsActionEquals(action));
+                if (listener == null)
+                {
+                    //todo: throw error instead?
+                    logger.Warn($"This action was not registered for '{eventType}' event.");
+                }
+                else
+                {
+                    listeners.Remove(listener);
+                }
+            }
+        }
+
+        private void WorkerLoop()
+        {
+            while (IsStarted())
+            {
+                if (workerWaitEvent.WaitOne(TimeSpan.FromMilliseconds(100)))
+                {
+                    OnTick();
+                }
+            }
+        }
+
+        private bool IsStarted()
+        {
+            lock (threadLock)
+            {
+                return started;
+            }
+        }
+
+        private void OnTick()
+        {
+            List<string> localFiredEvents;
+
+            lock (firedEventsLock)
+            {
+                localFiredEvents = firedEvents.ToList();
+                firedEvents.Clear();
+            }
+
+            foreach (string firedEvent in localFiredEvents)
+            {
+                if (!IsStarted())
+                {
+                    return;
+                }
+                ProcessEvent(firedEvent);
+            }
+        }
+
+        private void ProcessEvent(string eventType)
+        {
+            var localListeners = FetchListeners(eventType);
+
+            if (localListeners == null)
+            {
+                return;
+            }
+
+            foreach (EventListener listener in localListeners)
+            {
+                Action action = listener.Action;
+                try
+                {
+                    action();
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e, $"Listener failed to process '{eventType}' event.");
+                }
+            }
+        }
+
+        private List<EventListener> FetchListeners(string eventType)
+        {
+            List<EventListener> localListeners = null;
+
+            lock (listenersLock)
+            {
+                List<EventListener> listeners;
+                if (eventListeners.TryGetValue(eventType, out listeners))
+                {
+                    localListeners = new List<EventListener>(listeners);
+                }
+            }
+
+            return localListeners;
+        }
+
+        private class EventListener
+        {
+            private readonly Action action;
+
+            public EventListener(Action action)
+            {
+                this.action = action;
+            }
+
+            public Action Action
+            {
+                get { return action; }
+            }
+
+            public bool IsActionEquals(Action otherAction)
+            {
+                return action == otherAction;
+            }
+        }
+    }
+}
---
+++ Test.BitcoinUtilities.GUI.Models/TestEventManager.cs
@@ -0,0 +1,213 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using BitcoinUtilities.GUI.Models;
+using NUnit.Framework;
+using TestUtilities;
+
+namespace Test.BitcoinUtilities.GUI.Models
+{
+    [TestFixture]
+    public class TestEventManager
+    {
+        [Test]
+        public void TestSmoke()
+        {
+            MessageLog log = new MessageLog();
+            ActionPerformer performer = new ActionPerformer(log);
+            EventManager eventManager = new EventManager();
+
+            const string eventType1 = "ET1";
+            const string eventType2 = "ET2";
+
+            eventManager.Watch(eventType1, performer.Action1);
+            eventManager.Watch(eventType2, performer.Action2);
+
+            eventManager.Start();
+
+            log.Log($"Fired {eventType1} event.");
+            eventManager.Notify(eventType1);
+
+            Thread.Sleep(200);
+
+            log.Log($"Fired {eventType2} event.");
+            eventManager.Notify(eventType2);
+
+            Thread.Sleep(200);
+
+            Assert.That(log.GetLog(), Is.EqualTo(new string[]
+            {
+                $"Fired {eventType1} event.",
+                $"Action1 invoked.",
+                $"Fired {eventType2} event.",
+                $"Action2 invoked."
+            }));
+
+            // testing that attempt to remove non-existent event listeners does not affect existing watchers
+            eventManager.Unwatch(eventType1, performer.Action2);
+            eventManager.Unwatch(eventType2, performer.Action1);
+
+            log.Clear();
+            log.Log("Attempted to remove non-existent event listeners.");
+
+            log.Log($"Fired {eventType1} event.");
+            eventManager.Notify(eventType1);
+
+            Thread.Sleep(200);
+
+            log.Log($"Fired {eventType2} event.");
+            eventManager.Notify(eventType2);
+
+            Thread.Sleep(200);
+
+            Assert.That(log.GetLog(), Is.EqualTo(new string[]
+            {
+                $"Attempted to remove non-existent event listeners.",
+                $"Fired {eventType1} event.",
+                $"Action1 invoked.",
+                $"Fired {eventType2} event.",
+                $"Action2 invoked."
+            }));
+
+            eventManager.Unwatch(eventType1, performer.Action1);
+
+            log.Clear();
+            log.Log($"Removed event-listener for {eventType1}.");
+
+            log.Log($"Fired {eventType1} event.");
+            eventManager.Notify(eventType1);
+
+            log.Log($"Fired {eventType2} event.");
+            eventManager.Notify(eventType2);
+
+            Thread.Sleep(200);
+
+            Assert.That(log.GetLog(), Is.EqualTo(new string[]
+            {
+                $"Removed event-listener for {eventType1}.",
+                $"Fired {eventType1} event.",
+                $"Fired {eventType2} event.",
+                $"Action2 invoked."
+            }));
+        }
+
+        [Test]
+        public void TestSlowListener()
+        {
+            MessageLog log = new MessageLog();
+            ActionPerformer performer = new ActionPerformer(log);
+            EventManager eventManager = new EventManager();
+
+            const string eventType = "ET";
+
+            eventManager.Start();
+
+            eventManager.Watch(eventType, performer.SlowAction);
+
+            log.Log("Event fired.");
+            eventManager.Notify(eventType);
+
+            Thread.Sleep(150);
+
+            const int eventsCount = 10000;
+
+            Stopwatch sw = Stopwatch.StartNew();
+            log.Log($"Starting to fire a series of {eventsCount} events.");
+            for (int i = 0; i < eventsCount; i++)
+            {
+                eventManager.Notify(eventType);
+            }
+            log.Log($"Finished to fire a series of {eventsCount} events.");
+            Assert.That(sw.ElapsedMilliseconds, Is.LessThan(100));
+
+            Thread.Sleep(1000);
+
+            eventManager.Stop();
+
+            Assert.That(log.GetLog(), Is.EqualTo(new string[]
+            {
+                "Event fired.",
+                "SlowAction started.",
+                $"Starting to fire a series of {eventsCount} events.",
+                $"Finished to fire a series of {eventsCount} events.",
+                "SlowAction completed.",
+                "SlowAction started.",
+                "SlowAction completed."
+            }));
+        }
+
+        [Test]
+        public void TestBrokenListener()
+        {
+            MessageLog log = new MessageLog();
+            ActionPerformer performer1 = new ActionPerformer(log);
+            ActionPerformer performer2 = new ActionPerformer(log);
+            EventManager eventManager = new EventManager();
+
+            const string eventType = "ET";
+
+            eventManager.Start();
+
+            eventManager.Watch(eventType, performer1.BrokenAction);
+            eventManager.Watch(eventType, performer2.BrokenAction);
+
+            log.Log("Event fired.");
+            eventManager.Notify(eventType);
+
+            Thread.Sleep(200);
+
+            Assert.That(log.GetLog(), Is.EqualTo(new string[]
+            {
+                "Event fired.",
+                "BrokenAction invoked.",
+                "BrokenAction invoked."
+            }));
+
+            log.Log("Event fired.");
+            eventManager.Notify(eventType);
+
+            Thread.Sleep(200);
+
+            Assert.That(log.GetLog(), Is.EqualTo(new string[]
+            {
+                "Event fired.",
+                "BrokenAction invoked.",
+                "BrokenAction invoked.",
+                "Event fired.",
+                "BrokenAction invoked.",
+                "BrokenAction invoked."
+            }));
+        }
+
+        private class ActionPerformer
+        {
+            private readonly MessageLog log;
+
+            public ActionPerformer(MessageLog log)
+            {
+                this.log = log;
+            }
+
+            public void Action1()
+            {
+                log.Log("Action1 invoked.");
+            }
+
+            public void Action2()
+            {
+                log.Log("Action2 invoked.");
+            }
+
+            public void SlowAction()
+            {
+                log.Log("SlowAction started.");
+                Thread.Sleep(500);
+                log.Log("SlowAction completed.");
+            }
+
+            public void BrokenAction()
+            {
+                log.Log("BrokenAction invoked.");
+            }
+        }
+    }
+}
