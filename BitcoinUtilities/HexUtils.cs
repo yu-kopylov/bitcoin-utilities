12ba0b4d300a5f46b6eefd423206a127a0fff82d
Yuriy Kopylov <iyurij@gmail.com>
2017-09-24 15:56:12 +1000

    Added HexUtils.

---
+++ BitcoinUtilities/HexUtils.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace BitcoinUtilities
+{
+    public static class HexUtils
+    {
+        /// <summary>
+        /// Converts a byte array to a hexadecimal string.
+        /// </summary>
+        /// <param name="array">The byte array to convert.</param>
+        /// <returns>A hexadecimal string converted from the given array.</returns>
+        /// <exception cref="ArgumentNullException">If the given array is null.</exception>
+        public static string GetString(byte[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array), "The given array is null.");
+            }
+
+            char[] octets = new char[array.Length * 2];
+            int ofs = 0;
+            foreach (byte b in array)
+            {
+                octets[ofs] = GetOctet(b >> 4);
+                octets[ofs + 1] = GetOctet(b & 0xF);
+                ofs += 2;
+            }
+
+            return new string(octets);
+        }
+
+        /// <summary>
+        /// Converts a hexadecimal string to a byte array.
+        /// </summary>
+        /// <param name="hex">The string to convert.</param>
+        /// <param name="result">A byte array converted from the given string; or null if conversion faled.</param>
+        /// <returns>true if string was converted to bytes; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">If the given string is null.</exception>
+        public static bool TryGetBytes(string hex, out byte[] result)
+        {
+            result = null;
+
+            if (hex == null || hex.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            int arrayLength = hex.Length / 2;
+            byte[] array = new byte[arrayLength];
+
+            for (int i = 0, ofs = 0; i < arrayLength; i++, ofs += 2)
+            {
+                byte hi, low;
+                if (!TryParseOctet(hex[ofs], out hi) || !TryParseOctet(hex[ofs + 1], out low))
+                {
+                    return false;
+                }
+                array[i] = (byte) (hi << 4 | low);
+            }
+
+            result = array;
+            return true;
+        }
+
+        /// <summary>
+        /// This method does not validate parameters. It is responsibility of the caller to pass values in range from 0 to 15.
+        /// </summary>
+        private static char GetOctet(int octetValue)
+        {
+            if (octetValue < 10)
+            {
+                return (char) ('0' + octetValue);
+            }
+            return (char) ('a' + octetValue - 10);
+        }
+
+        private static bool TryParseOctet(char octet, out byte octetValue)
+        {
+            octetValue = 0;
+
+            if (octet >= '0' && octet <= '9')
+            {
+                octetValue = (byte) (octet - '0');
+                return true;
+            }
+
+            if (octet >= 'a' && octet <= 'f')
+            {
+                octetValue = (byte) (octet - 'a' + 10);
+                return true;
+            }
+
+            if (octet >= 'A' && octet <= 'F')
+            {
+                octetValue = (byte) (octet - 'A' + 10);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Test.BitcoinUtilities/TestHexUtils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using BitcoinUtilities;
+using NUnit.Framework;
+
+namespace Test.BitcoinUtilities
+{
+    [TestFixture]
+    public class TestHexUtils
+    {
+        [Test]
+        public void TestGetString()
+        {
+            Assert.Throws<ArgumentNullException>(() => HexUtils.GetString(null));
+
+            Assert.That(HexUtils.GetString(new byte[0]), Is.EqualTo(""));
+            Assert.That(HexUtils.GetString(new byte[] {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF}), Is.EqualTo("0123456789abcdef"));
+        }
+
+        [Test]
+        public void TestTryGetBytes()
+        {
+            byte[] bytes;
+
+            Assert.False(HexUtils.TryGetBytes(null, out bytes));
+            Assert.That(bytes, Is.Null);
+
+            Assert.False(HexUtils.TryGetBytes("a", out bytes));
+            Assert.That(bytes, Is.Null);
+
+            Assert.False(HexUtils.TryGetBytes("aaa", out bytes));
+            Assert.That(bytes, Is.Null);
+
+            Assert.False(HexUtils.TryGetBytes("0Z", out bytes));
+            Assert.That(bytes, Is.Null);
+
+            Assert.False(HexUtils.TryGetBytes("Z0", out bytes));
+            Assert.That(bytes, Is.Null);
+
+            Assert.True(HexUtils.TryGetBytes("", out bytes));
+            Assert.That(bytes, Is.EqualTo(new byte[0]));
+
+            Assert.True(HexUtils.TryGetBytes("01", out bytes));
+            Assert.That(bytes, Is.EqualTo(new byte[] {0x01}));
+
+            Assert.True(HexUtils.TryGetBytes("FE", out bytes));
+            Assert.That(bytes, Is.EqualTo(new byte[] {0xFE}));
+
+            Assert.True(HexUtils.TryGetBytes("fe", out bytes));
+            Assert.That(bytes, Is.EqualTo(new byte[] {0xFE}));
+
+            Assert.True(HexUtils.TryGetBytes("0123456789ABCDEF", out bytes));
+            Assert.That(bytes, Is.EqualTo(new byte[] {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF}));
+
+            Assert.True(HexUtils.TryGetBytes("0123456789abcdef", out bytes));
+            Assert.That(bytes, Is.EqualTo(new byte[] {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF}));
+        }
+    }
+}
