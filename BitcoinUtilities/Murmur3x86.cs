6a7e2bcb8bcf291c0878f02b414476b799729fa6
Yuriy Kopylov <iyurij@gmail.com>
2019-03-10 10:56:49 +1100

    Added Murmur3x86.

---
+++ BitcoinUtilities/Murmur3x86.cs
@@ -0,0 +1,57 @@
+﻿namespace BitcoinUtilities
+{
+    public static class Murmur3x86
+    {
+        public static uint GetHash32(uint nHashSeed, byte[] data)
+        {
+            int len = data.Length;
+            uint h1 = nHashSeed;
+
+            const uint c1 = 0xCC9E2D51;
+            const uint c2 = 0x1B873593;
+
+            int ofs = 0;
+            for (; ofs + 3 < len; ofs += 4)
+            {
+                uint k1 = (uint) (data[ofs] | data[ofs + 1] << 8 | data[ofs + 2] << 16 | data[ofs + 3] << 24);
+
+                k1 *= c1;
+                k1 = RotateLeft32(k1, 15);
+                k1 *= c2;
+
+                h1 ^= k1;
+                h1 = RotateLeft32(h1, 13);
+                h1 = h1 * 5 + 0xE6546B64;
+            }
+
+            if (ofs < len)
+            {
+                uint k1 = 0;
+
+                for (int tailOfs = len - 1; tailOfs >= ofs; tailOfs--)
+                {
+                    k1 = k1 << 8 | data[tailOfs];
+                }
+
+                k1 *= c1;
+                k1 = RotateLeft32(k1, 15);
+                k1 *= c2;
+                h1 ^= k1;
+            }
+
+            h1 ^= (uint) len;
+            h1 ^= h1 >> 16;
+            h1 *= 0x85ebca6b;
+            h1 ^= h1 >> 13;
+            h1 *= 0xc2b2ae35;
+            h1 ^= h1 >> 16;
+
+            return h1;
+        }
+
+        private static uint RotateLeft32(uint value, int bits)
+        {
+            return (value << bits) | (value >> (32 - bits));
+        }
+    }
+}
---
+++ Test.BitcoinUtilities/TestMurmur3x86.cs
@@ -0,0 +1,30 @@
+﻿using BitcoinUtilities;
+using NUnit.Framework;
+
+namespace Test.BitcoinUtilities
+{
+    [TestFixture]
+    public class TestMurmur3x86
+    {
+        [Test]
+        public void TestGetHash32()
+        {
+            Assert.AreEqual(0x00000000u, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("")));
+            Assert.AreEqual(0x6A396F08u, Murmur3x86.GetHash32(0xFBA4C795, HexUtils.GetBytesUnsafe("")));
+            Assert.AreEqual(0x81F16F39u, Murmur3x86.GetHash32(0xffffffff, HexUtils.GetBytesUnsafe("")));
+
+            Assert.AreEqual(0x514E28B7u, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("00")));
+            Assert.AreEqual(0xEA3F0B17u, Murmur3x86.GetHash32(0xFBA4C795, HexUtils.GetBytesUnsafe("00")));
+            Assert.AreEqual(0xFD6CF10Du, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("ff")));
+
+            Assert.AreEqual(0x16C6B7ABu, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("0011")));
+            Assert.AreEqual(0x8EB51C3Du, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("001122")));
+            Assert.AreEqual(0xB4471BF8u, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("00112233")));
+            Assert.AreEqual(0xE2301FA8u, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("0011223344")));
+            Assert.AreEqual(0xFC2E4A15u, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("001122334455")));
+            Assert.AreEqual(0xB074502Cu, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("00112233445566")));
+            Assert.AreEqual(0x8034D2A0u, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("0011223344556677")));
+            Assert.AreEqual(0xB4698DEFu, Murmur3x86.GetHash32(0x00000000, HexUtils.GetBytesUnsafe("001122334455667788")));
+        }
+    }
+}
